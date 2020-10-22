74b237b2dc7e065ca96a90e3d294dfa8838c1663
Tom Chance <tom.chance@alion.nl>
2020-10-22 19:51:30 +0200

    Script implementation for basic Role generator

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,18 @@
-function actSus() {
-    fetch("assets/sus.json")
-        .then(response => response.json())
-        .then(json => console.log(json));
+async function init() {
+    var response = await fetch("assets/sus.json");
+    return await response.json();
+}
+
+var suslist = [];
+init().then(response => suslist = response);
+
+async function actSus() {
+    var randomIndex = Math.floor((Math.random() * suslist.roles.length));
+    
+    var randomRole = suslist.roles[randomIndex];
+    var roleTitle = randomRole.roleName;
+    var roleDescription = randomRole.roleDescription;
+
+    document.querySelector('#role-name').innerHTML = roleTitle;
+    document.querySelector('#role-description').innerHTML = roleDescription;
 }
